a6580d8421c0c09b6e26644064f19306b83fb5a2
Rick Sun <liamcloud@protonmail.com>
2023-01-28 16:22:15 +0100

    nav

--- pgessays/pages/index.js
+++ pgessays/pages/index.js
@@ -8,12 +8,21 @@ import adam from '../public/adam.png'
 import essays from './essays.json'
 import { useState } from 'react'
 import Link from 'next/link'
+import { NodeNextResponse } from 'next/dist/server/base-http/node'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-  const [category, setCategory] = useState('startups')
-  const [essayId, setEssayId] = useState(2)
+  const [category, setCategory] = useState("Latest")
+  const [latest, setLatest] = useState(true)
+  const [startups, setStartups] = useState(false)
+  const updateCat = () => {
+    setCategory("startups")
+    setLatest(false)
+    setStartups(true)
+  }
+  const updateCat2 = () => {
+    setCategory("family")
+  }
   const myStyle = {
     backgroundImage:
       "url('https://www.dropbox.com/s/qd5o7hito845ohi/DALL%C2%B7E%202023-01-27%2013.32.23%20-%20stained%20glass%20window%20of%20a%20painting%20by%20michelangelo%20of%20mans%20thinking%20about%20philosophy%2C%20renaissance%20vibe%2C%20baroque%20vibe%20%281%29.png?raw=1')"
@@ -38,9 +47,9 @@ export default function Home() {
         </div>
         <div className="menu">
           <ul>
-            <li className="example">Latest</li>
-            <li>Startups</li>
-            <li>Family</li>
+            <li className={latest ? "example" : "none"}>Latest</li>
+            <li onClick={updateCat} className={startups ? "example" : "none"}>Startups</li>
+            <li onClick={updateCat2}>Family</li>
             <li>Latest</li>
             <li>Latest</li>
           </ul>
