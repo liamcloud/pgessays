611817098e496e3fdd23fc662d55f1e969c14fcf
Rick Sun <liamcloud@protonmail.com>
2023-02-23 01:36:38 +0100

    inter

--- pgessays/pages/index.js
+++ pgessays/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { GiSoundOn } from 'react-icons/gi'
 import { GiSoundOff } from 'react-icons/gi'
